8d663beca823f83b0e072f09326d34b8d68090b2
Ray Antonius <rayantonius@gmail.com>
2018-12-10 20:37:02 +0700

    Finalizing Simulink

--- runArduino.m
+++ runArduino.m
@@ -4,7 +4,7 @@ throttleOutput = [0;0];
 
 set_param('Model', 'SimulationCommand', 'start')
 
-samples = 100; %number of samples
+samples = 1000; %number of samples
 min = 1000;
 max = 2000;
 
@@ -14,12 +14,21 @@ time = double(nan(1, samples));
 
 s1 = serial('COM4'); %Insert com port here
 
-set(s1, 'baudrate', 115200);     % See List Below
-set(s1, 'parity', 'n');        % 'n' or 'y'
-set(s1, 'stopbits', 1);        % 1 or 2
-set(s1, 'timeout', 123);     % 12.3 Seconds as an example here
+set(s1, 'baudrate', 250000);     % parameter settings for serial connections
+set(s1, 'parity', 'n');
+set(s1, 'stopbits', 1);       
+set(s1, 'timeout', 123);     
 
 fopen(s1);
+
+set_param('Model', 'SimulationCommand', 'pause');
+pause(7);
+set_param('Model', 'SimulationCommand', 'continue');
+
+%set initial PID values
+set_param('Model/Controller', 'P', num2str(2.7));
+set_param('Model/Controller', 'I', num2str(0.006));
+set_param('Model/Controller', 'D', num2str(1.4));
 
 subplot(2,1,1);
 plotGraph = plot(time, angle, '-r');
@@ -35,10 +44,10 @@ tic
 for x=1:samples
     time(x) = toc;
 
-    angle(x) = fscanf(s1, '%d\n');
-    set_param(angleBlock,'Value',num2str(angle(x))) %send a to the model
-    pwm(x) = fscanf(s1, '%d\n');
-    set_param(throttBlock,'Value',num2str(pwm(x))) %send a to the model
+    angle(x) = fscanf(s1, '%f\n');
+    set_param(angleBlock,'Value',num2str(angle(x))) %send data to the model
+    pwm(x) = fscanf(s1, '%f\n');
+    set_param(throttBlock,'Value',num2str(pwm(x))) %send data to the model
     
     subplot(2,1,1);
     set(plotGraph1, 'XData', time, 'YData', pwm);
@@ -46,9 +55,9 @@ for x=1:samples
     subplot(2,1,2);
     set(plotGraph, 'XData', time, 'YData', angle);
     
-    %fprintf('%.2f\n', throttleOutput);    
+    fprintf(s1, '%.2f\n', throttleOutput);    %sends data back to arduino
     
-    pause(0.1); %frequency
+    pause(0.005); %frequency
 end
 
 set_param('Model', 'SimulationCommand', 'stop');
